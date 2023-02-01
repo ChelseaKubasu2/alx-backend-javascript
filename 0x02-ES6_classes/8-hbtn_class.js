5ae0025b421da274e973cab654ef23b4c844f253
ChelseaKubasu2 <kubasuchelsea@gmail.com>
2023-02-01 18:20:46 +0000

    add files

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,25 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
+    console.log(this);
+  }
+
+  get size() {
+    return this._size;
+  }
+
+  get location() {
+    return this._location;
+  }
+
+  [Symbol.toPrimitive](hint) {
+    if (hint === 'string') {
+      return this._location;
+    }
+    if (hint === 'number') {
+      return this._size;
+    }
+    return null;
+  }
+}
