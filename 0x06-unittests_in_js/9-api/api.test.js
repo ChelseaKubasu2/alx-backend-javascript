59b4e3d368baf83f87d598200f87ffd6408301db
ChelseaKubasu2 <kubasuchelsea@gmail.com>
2023-02-23 19:23:27 +0000

    add files

---
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,55 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const { expect } = require('chai');
+
+chai.use(chaiHttp);
+
+describe('API', () => {
+  describe('GET /', () => {
+    it('should return status code 200', () => {
+      chai.request('http://localhost:7865')
+      .get('/')
+      .end((err, res) => {
+        if (err) throw err;
+        expect(res.statusCode).to.equal(200);
+      })
+    });
+
+    it('should return the correct message', () => {
+      chai.request('http://localhost:7865')
+      .get('/')
+      .end((err, res) => {
+        if (err) throw err;
+        expect(res.text).to.equal('Welcome to the payment system');
+      })
+    });
+    it('should return status code 404 on wrong endpoint', () => {
+      chai.request('http://localhost:7865')
+      .get('/hmm')
+      .end((err, res) => {
+        if (err) throw err;
+        expect(res.statusCode).to.equal(404);
+      })
+    });
+  })
+
+  describe('GET /cart/:id', () => {
+    it('should return the correct status code and message', () => {
+      chai.request('http://localhost:7865')
+      .get('/cart/12')
+      .end((err, res) => {
+        if (err) throw err;
+        expect(res.statusCode).to.equal(200);
+        expect(res.text).to.equal(`Payment methods for cart 12`);
+      });
+    });
+    it('should return 404 for a non-number id', () => {
+      chai.request('http://localhost:7865')
+      .get('/cart/hello -v')
+      .end((err, res) => {
+        if (err) throw err;
+        expect(res.statusCode).to.equal(404);
+      });
+    })
+  })
+})
